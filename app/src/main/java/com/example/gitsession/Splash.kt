c68265f33658b28469916b500964ebdeabf4d290
hossam <hossamkotp89@gmail.com>
2020-02-13 15:07:13 +0200

    modification in Splash Activity

--- app/src/main/java/com/example/gitsession/Splash.kt
+++ app/src/main/java/com/example/gitsession/Splash.kt
@@ -8,5 +8,6 @@ class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
     }
 }
